63c9e87f504aadd8e91751e85dcec27d205d82ce
MarioGN <mario.geroldineto@gmail.com>
2019-10-03 01:28:41 -0300

    Primeiro commit, backend da aplicaçao criado. Rotas pararegistro de usuario, login, cadastro de spots e rotas para vizualizaçao dos dados

---
+++ backend/src/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const routes = require('./routes');
+
+require('dotenv').config();
+
+
+const app = express();
+
+mongoose.connect(`mongodb+srv://${process.env.DB_PASS}:${process.env.DB_PASS}@cluster0-z206s.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+app.use(express.json());
+app.use(routes);
+
+app.listen(process.env.PORT);
