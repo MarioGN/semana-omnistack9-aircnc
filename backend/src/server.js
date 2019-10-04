41d0dbbec58f2b073363854f81bcd6529d265d06
MarioGN <mario.geroldineto@gmail.com>
2019-10-03 22:10:18 -0300

    Criando o frontend da aplicaçao utilizando reactjs. Correçao no metodo store da controller de spots no backend

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
+const path = require('path');
+
 const routes = require('./routes');
 
 require('dotenv').config();
-
 
 const app = express();
 
@@ -12,7 +14,9 @@ mongoose.connect(`mongodb+srv://${process.env.DB_PASS}:${process.env.DB_PASS}@cl
   useUnifiedTopology: true
 });
 
+app.use(cors());
 app.use(express.json());
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
 
 app.listen(process.env.PORT);
