b0a6a01d45faa361613a9eb2c3d473f9775ef245
MarioGN <mario.geroldineto@gmail.com>
2019-10-06 22:52:36 -0300

    Criando funcionalidade de comunicação realtime utilizando socket.io

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,15 +3,35 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const path = require('path');
 
+const socketio = require('socket.io');
+const http = require('http');
+
 const routes = require('./routes');
 
 require('dotenv').config();
 
 const app = express();
+const server = http.Server(app);
+const io = socketio(server);
 
 mongoose.connect(`mongodb+srv://${process.env.DB_PASS}:${process.env.DB_PASS}@cluster0-z206s.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`, {
   useNewUrlParser: true,
   useUnifiedTopology: true
+});
+
+const connectedUsers = {};
+
+io.on('connection', socket => {
+  const { user_id } = socket.handshake.query;
+  connectedUsers[user_id] = socket.id;
+});
+
+// Middleware para disponibilizar io e os usuários conectados
+app.use((req, res, next) => {
+  req.io = io;
+  req.connectedUsers = connectedUsers;
+
+  return next();
 });
 
 app.use(cors());
@@ -19,4 +39,4 @@ app.use(express.json());
 app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
 
-app.listen(process.env.PORT);
+server.listen(process.env.PORT);
